f6f612e531ea6234872c6a1fb10b1669bbd171db
JevSuchoi <jev.suchoi@minbuza.onmicrosoft.com>
2020-10-02 18:01:40 +0200

    Added git installer

--- 2020-10_azure_bicep/Install-vmFeatures.ps1
+++ 2020-10_azure_bicep/Install-vmFeatures.ps1
@@ -23,10 +23,15 @@ function Install-vmFeatures {
         [switch] $SkipPwshExtention,
 
         [Parameter()]
-        [switch] $SkipAzModule
+        [switch] $SkipAzModule,
+
+        [Parameter()]
+        [switch] $SkipGit
     )
 
     begin {
+        $repoRoot = $PSScriptRoot
+
         # Set information level to show output messages
         $InformationPreference = 'continue'
         $ErrorActionPreference = 'stop'
@@ -51,6 +56,14 @@ function Install-vmFeatures {
         $vsixPath = "$env:TEMP\vscode-bicep.vsix"
         (New-Object Net.WebClient).DownloadFile("https://github.com/Azure/bicep/releases/latest/download/vscode-bicep.vsix", $vsixPath)
 
+        $gitVersions = (Invoke-RestMethod "https://api.github.com/repos/git-for-windows/git/releases/latest").assets
+        $desiredVersion = $gitVersions | Where-Object { $_.name -like "*64-bit.exe" }  
+
+        # Fetch the latest Bicep CLI binary
+        Write-Information "Downloading Git.exe"
+        $gitPath = "$env:TEMP\Git-64-bit.exe"
+        (New-Object Net.WebClient).DownloadFile($($desiredVersion.browser_download_url), $gitPath)
+
         $codeCmdPath = "$env:USERPROFILE\AppData\Local\Programs\Microsoft VS Code\bin\code.cmd"
     }
 
@@ -65,19 +78,19 @@ function Install-vmFeatures {
         if ($SkipVSCode.IsPresent -eq $false) {
             Write-Information "Installing VSCode"
             Start-Process -Wait $vsCodePath -ArgumentList /silent, /mergetasks=!runcode
-        } else {
+        }
+        else {
             Write-Information "SKIPPING: Installing VSCode"
         }
-
 
         if ($SkipPwshExtention.IsPresent -eq $false) {
             # Installing extension extentions
             Write-Information "Installing vscode PowerShell extention"
             & $codeCmdPath --install-extension  ms-vscode.powershell
-        } else {
+        }
+        else {
             Write-Information "SKIPPING: Installing vscode PowerShell extention"
         }
-
 
         Write-Information "Installing vscode Bicep extention"
         & $codeCmdPath --install-extension $vsixPath
@@ -86,10 +99,14 @@ function Install-vmFeatures {
             # install PowerShell AZ module
             Write-Information "Installing AZ Module"
             Install-Module -Name Az -Repository PSGallery -Force -Verbose
-        } else {
+        }
+        else {
             Write-Information "SKIPPING: Installing AZ Module"
         }
 
+        if ($SkipGit.IsPresent -eq $false) {
+            Start-Process -Wait $gitPath -ArgumentList /VERYSILENT, /NORESTART, /NOCANCEL, /LOADINF="$repoRoot\git.inf"
+        }
     }
 
     end {
